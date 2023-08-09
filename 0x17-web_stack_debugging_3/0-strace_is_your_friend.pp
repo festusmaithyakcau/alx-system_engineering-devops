680025bc78d47d17b9034451e4b6315fae135565
festusmaithyakcau <festusmaithya264@gmail.com>
2023-08-09 13:26:15 -0700

    Fixed the Apache 500 error by updating the PHP module misconfig

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,13 @@
-# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+# 0-strace_is_your_friend.pp:
+# manifest - fix Apache 500 error by updating the PHP module misconfig
 
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
+}
+
+# notify Apache service to reload after PHP module is updated:
+service { 'apache2':
+  ensure => 'running',
+  notify => Exec['fix-wordpress'],
 }
